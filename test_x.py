5284b68d1b49dd5f758af9586f0ab60bf6a1c3f0
aph <a.hewson@gmail.com>
2014-03-16 22:06:52 +0000

    refactor tests

--- test_x.py
+++ test_x.py
@@ -16,44 +16,38 @@ def test_does_nothing_to_unknown_file(tmpdir):
     assert paths(tmpdir) == set(['foo'])
 
 
-def test_untars_rooted(tmpdir):
+def extract_test(tmpdir, archive, files):
     tmpdir.chdir()
-    test_dir.join('rooted.tar').copy(tmpdir.join('rooted.tar'))
-    assert main(['rooted.tar']) == 0
-    assert paths(tmpdir) == set(['rooted.tar', 'root', 'root/a', 'root/b'])
+    test_dir.join(archive).copy(tmpdir.join(archive))
+    assert main([archive]) == 0
+    assert paths(tmpdir) == set(files + [archive])
+
+
+def test_untars_rooted(tmpdir):
+    extract_test(tmpdir, 'rooted.tar',
+                 ['root', 'root/a', 'root/b'])
 
 
 def test_untars_rooted_with_conflict(tmpdir):
-    tmpdir.chdir()
     tmpdir.mkdir('root')
     tmpdir.join('root', 'foo').write('bar')
-    test_dir.join('rooted.tar').copy(tmpdir.join('rooted.tar'))
-    assert main(['rooted.tar']) == 0
-    assert paths(tmpdir) == set([
-        'rooted.tar', 'root', 'root/foo', 'root-1', 'root-1/a', 'root-1/b'])
+    extract_test(tmpdir, 'rooted.tar',
+                 ['root', 'root/foo', 'root-1', 'root-1/a', 'root-1/b'])
 
 
 def test_untars_unrooted(tmpdir):
-    tmpdir.chdir()
-    test_dir.join('unrooted.tar').copy(tmpdir.join('unrooted.tar'))
-    assert main(['unrooted.tar']) == 0
-    assert paths(tmpdir) == set([
-        'unrooted.tar', 'unrooted', 'unrooted/a', 'unrooted/b'])
+    extract_test(tmpdir, 'unrooted.tar',
+                 ['unrooted', 'unrooted/a', 'unrooted/b'])
 
 
 def test_untars_unrooted_conflict(tmpdir):
-    tmpdir.chdir()
     tmpdir.mkdir('unrooted')
     tmpdir.join('unrooted', 'foo').write('bar')
-    test_dir.join('unrooted.tar').copy(tmpdir.join('unrooted.tar'))
-    assert main(['unrooted.tar']) == 0
-    assert paths(tmpdir) == set([
-        'unrooted.tar', 'unrooted', 'unrooted/foo',
-        'unrooted-1', 'unrooted-1/a', 'unrooted-1/b'])
+    extract_test(tmpdir, 'unrooted.tar',
+                 ['unrooted', 'unrooted/foo',
+                  'unrooted-1', 'unrooted-1/a', 'unrooted-1/b'])
 
 
 def test_untars_bz2(tmpdir):
-    tmpdir.chdir()
-    test_dir.join('rooted.tar.bz2').copy(tmpdir.join('rooted.tar.bz2'))
-    assert main(['rooted.tar.bz2']) == 0
-    assert paths(tmpdir) == set(['rooted.tar.bz2', 'root', 'root/a', 'root/b'])
+    extract_test(tmpdir, 'rooted.tar.bz2',
+                 ['root', 'root/a', 'root/b'])
